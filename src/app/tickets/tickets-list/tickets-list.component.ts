949425164e33a9d472e6955c052d1360317a968f
Juan David Jaramillo <djaramilloj1@gmail.com>
2020-12-04 15:54:25 -0500

    fixing date format on tickets

--- src/app/tickets/tickets-list/tickets-list.component.ts
+++ src/app/tickets/tickets-list/tickets-list.component.ts
@@ -8,6 +8,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import {
     ClosedTicketDialogComponent
   } from '../../../app/material-component/closedticket-dialog/closedticket-dialog.component';
+import { NativeDateAdapter, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { formatDate } from '@angular/common';
 
 export interface TicketElement {
   id: string;
@@ -30,12 +32,36 @@ export class currentChatData {
     companyId: string;
   }
 
+export const PICK_FORMATS = {
+parse: {dateInput: {month: 'short', year: 'numeric', day: 'numeric'}},
+display: {
+    dateInput: 'input',
+    monthYearLabel: {year: 'numeric', month: 'short'},
+    dateA11yLabel: {year: 'numeric', month: 'long', day: 'numeric'},
+    monthYearA11yLabel: {year: 'numeric', month: 'long'}
+}
+};
+
+class PickDateAdapter extends NativeDateAdapter {
+format(date: Date, displayFormat: Object): string {
+    if (displayFormat === 'input') {
+        return formatDate(date,'dd-MMM-yyyy',this.locale);;
+    } else {
+        return date.toDateString();
+    }
+}
+}
+
 let TICKETS: TicketElement[] = [];
 
 @Component({
   selector: 'app-tickets-list',
   templateUrl: './tickets-list.component.html',
-  styleUrls: ['./tickets-list.component.scss']
+  styleUrls: ['./tickets-list.component.scss'],
+  providers: [
+    {provide: DateAdapter, useClass: PickDateAdapter},
+    {provide: MAT_DATE_FORMATS, useValue: PICK_FORMATS}
+  ]
 })
 
 
