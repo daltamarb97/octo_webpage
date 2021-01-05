04d091c8ecb8db6d7d9b2b29ac76dd7a6552f511
Juan David Jaramillo <djaramilloj1@gmail.com>
2021-01-05 12:59:11 -0500

    ready for deploy of orders

--- src/app/shared/menu-items/menu-items.ts
+++ src/app/shared/menu-items/menu-items.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {
+  HoldDataService
+} from '../../core/services/hold-data.service';
 
 export interface Menu {
   state: string;
@@ -7,8 +10,20 @@ export interface Menu {
   icon: string;
 }
 
-const MENUITEMS = [
+const MENUITEMSORDERS = [
   { state: 'pedidos', type: 'link', name: 'Pedidos', icon: 'fact_check' },
+  { state: 'whatsapp', type: 'link', name: 'WhatsApp', icon: 'chat' },
+  { state: 'chat-flow', type: 'link', name: 'Octo-Bot', icon: 'account_tree' },
+  { state: 'tag-metrics', type: 'link', name: 'Estadísticas', icon: 'show_chart' },
+  { state: 'tickets', type: 'link', name: 'Tickets', icon: 'fact_check' },
+  { state: 'forms', type: 'link', name: 'Encuestas', icon: 'format_quote' },
+  { state: 'canales-comunicacion', type: 'link', name: 'chats', icon: 'message' },
+  { state: 'directorio', type: 'link', name: 'Directorio', icon: 'book' },
+  { state: 'perfil', type: 'link', name: 'Perfil', icon: 'account_circle' },
+
+];
+
+const MENUITEMS = [
   { state: 'whatsapp', type: 'link', name: 'WhatsApp', icon: 'chat' },
   { state: 'chat-flow', type: 'link', name: 'Octo-Bot', icon: 'account_tree' },
   { state: 'tag-metrics', type: 'link', name: 'Estadísticas', icon: 'show_chart' },
@@ -22,7 +37,12 @@ const MENUITEMS = [
 
 @Injectable()
 export class MenuItems {
+  constructor(private holdData: HoldDataService) {}
   getMenuitem(): Menu[] {
-    return MENUITEMS;
+    if (this.holdData.companyInfo.delivery) {
+      return MENUITEMSORDERS;
+    } else {
+      return MENUITEMS;
+    }
   }
 }
